ae10f4c8359a66f9118007ceeb98280f08f81ca6
abskr82 <abhishekkumar647@outlook.com>
2018-04-08 00:30:26 +0530

    Lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { EagerComponent } from './eager/eager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
