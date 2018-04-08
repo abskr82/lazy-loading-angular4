330befc97ab98009c7db773e7056f060a5945b0a
abskr82 <abhishekkumar647@outlook.com>
2018-04-08 11:18:30 +0530

    ng eject

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,8 @@ import { EagerComponent } from './eager/eager.component';
 const routes: Routes = [
   { path: '', redirectTo: 'eager', pathMatch: 'full' },
   { path: 'eager', component: EagerComponent },
-  { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' }
+//   { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' },
+  { path: 'lazy', loadChildren: '/home/abhishek/programming/angular4/lazy-loading/lazy2/src/app/lazy/lazy.module#LazyModule' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
